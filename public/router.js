1cc531fee1d8e50796103fedf0148249baa7f9bb
Anthony Perez <jperez-zepeda@csumb.edu>
2019-12-08 10:32:34 -0800

    made home page better w/ bootstrap

--- public/router.js
+++ public/router.js
@@ -1,11 +1,35 @@
 var express = require('express');
 const router = express.Router();
-//const mysql = require('mysql');
+const mysql = require('mysql');
 
 router.get('/', function(req, res, next){
-    res.render('../public/home', {
-        title: 'Gaming Tournaments'
+    
+    const sql = `
+SELECT * FROM tournament;
+`;
+
+    const connection = mysql.createConnection({
+        host: 'z1ntn1zv0f1qbh8u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'gvoch3v86kyzmy53',
+        password: 'hmrcywyic6i7uni5',
+        database: 'sp1hoq0zi7n09fn5'
     });
+
+    connection.connect();
+
+    connection.query(sql, (error, results, fields) => {
+        if (error) throw error;
+
+        res.render('../public/home', {
+            title: 'Gaming Tournaments',
+            tournaments: results
+        });
+        
+    });
+
+    connection.end();
+    
+    
 })
 
 module.exports = router;
