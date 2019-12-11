75e987d012c34960d53663db8b015b8b3989c72a
Anthony Perez <jperez-zepeda@csumb.edu>
2019-12-10 20:23:30 -0800

    finished join page

--- public/router.js
+++ public/router.js
@@ -76,8 +76,6 @@ router.get('/view', function(req, res, next){
 
     }
     
-
-    
     
 })
 
@@ -153,9 +151,77 @@ router.post('/login', function(req, res, next){
 })
 
 router.get('/join', function(req, res, next){
-    res.render('../public/login',{
-        title: 'Login'
-    })
+
+    const id = req.query.id
+
+    if(id){
+        const sql = `
+        SELECT *
+        FROM tournament 
+        WHERE tournament.id = ${id};
+    `;
+        const connection = mysql.createConnection({
+            host: 'z1ntn1zv0f1qbh8u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            user: 'gvoch3v86kyzmy53',
+            password: 'hmrcywyic6i7uni5',
+            database: 'sp1hoq0zi7n09fn5'
+        });
+    
+        connection.connect();
+    
+        connection.query(sql, (error, results, fields) => {
+            if (error) throw error;
+            
+            console.log("results:", results)
+
+            res.render('../public/join',{
+                title: 'Join Tournament',
+                data: results
+            })
+            
+            
+        });
+    
+        connection.end();
+
+    }
+    
+})
+
+router.post('/join', function(req, res, next){
+    
+    const username = req.body.username;
+    const password = req.body.password;
+    const id = req.body.id;
+
+    
+    const connection = mysql.createConnection({
+        host: 'z1ntn1zv0f1qbh8u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'gvoch3v86kyzmy53',
+        password: 'hmrcywyic6i7uni5',
+        database: 'sp1hoq0zi7n09fn5'
+    });
+
+    connection.connect();
+
+    connection.query(
+        'UPDATE user SET tournament_id = ?', [id]
+    , (error, results, fields) => {
+        if (error) throw error;
+        
+        console.log("results:", results)
+
+        res.render('../public/join',{
+            title: 'Join Tournament',
+            data: results
+        })
+        
+        
+    });
+
+    connection.end();
+
+    
 })
 
 module.exports = router;
